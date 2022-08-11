4dd11dec26f148cd23cdeb7676e5d20bfe558669
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2022-08-11 15:00:09 +0200

    TODOs

--- src/mentor/restore.py
+++ src/mentor/restore.py
@@ -11,7 +11,8 @@ def save(fname,net):
 def resume_classification(args, fname):
         net = create_classification_model(archname=args.arch, n_classes=args.n_classes, pretrained=args.pretrained, freeze_layers_before=args.freeze_all_before, device=args.device)
         try:
-                save_dict = torch.load(open(fname,"rb"))
+                save_dict = torch.load(open(fname,"rb"), map_location=torch.device(args.device))
+                net.load_state_dict(save_dict["weights"])
                 new_epoch = len(save_dict["train_history"])
                 save_dict["args_history"][new_epoch] = args
                 net.status = save_dict["status"]
@@ -19,6 +20,7 @@ def resume_classification(args, fname):
                 net.train_history = save_dict["train_history"]
                 net.validation_history = save_dict["validation_history"]
                 net.best_weights = save_dict["best_weights"]
+
         except FileNotFoundError:
                 warn(f"could not load {fname}")
         return net
